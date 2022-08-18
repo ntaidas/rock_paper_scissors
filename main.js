ff676a6c123ba32e3a9b6c6a2037e9def96d170b
AidasD <ntaidas@gmail.com>
2022-08-18 20:09:48 +0100

    no more infinite playAgain

--- main.js
+++ main.js
@@ -26,63 +26,26 @@ playerScore.innerText = 0
 enemyScore.innerText  = 0
 let playerScoreCounter = 0
 let enemyScoreCounter = 0
+let playAgain = document.createElement("button")
 
-    function gameOver(){
-        if (playerScoreCounter == 5){
-            h1.innerText = 'You saved the day!'
-            paper.removeEventListener('click',playPaper)
-            rock.removeEventListener('click', playRock)
-            scissors.removeEventListener('click', playScissors)
-            reset()
-        }
-        if (enemyScoreCounter == 5){
-            h1.innerText = 'Abbandon all hope!!!'
-            paper.removeEventListener('click',playPaper)
-            rock.removeEventListener('click', playRock)
-            scissors.removeEventListener('click', playScissors)
-            reset()
-        }
-    }
-
-    function reset(){
-        let playAgain = document.createElement("button")
-        playAgain.innerText= "Play Again"
-        playAgain.setAttribute("id", "play-again")
-        document.body.appendChild(playAgain)
-    }
-
-    function loosing(){
-        playerScore.style.fontSize = "50px"
-        enemyScore.style.fontSize = "60px"
-    }
-    function winning(){
-        playerScore.style.fontSize = "60px"
-        enemyScore.style.fontSize = "50px"
-    }
-
-    function tie(){
-        playerScore.style.fontSize = "50px"
-        enemyScore.style.fontSize = "50px"       
-    }
- 
     function playRock(){
-            let computerSelection = computerPlay()
-            if(computerSelection == ('paper')){
-                h1.innerText = loose
-                enemyScore.innerText++
-                enemyScoreCounter++
-                loosing()
-            }
-            else if (computerSelection == ('scissors')){
-                h1.innerText = win
-                playerScore.innerText++
-                playerScoreCounter++
-                winning()  
-            }
-            else {
-                h1.innerText = draw
-            }
-            gameOver()
+        let computerSelection = computerPlay()
+        if(computerSelection == ('paper')){
+            h1.innerText = loose
+            enemyScore.innerText++
+            enemyScoreCounter++
+            loosing()
+        }
+        else if (computerSelection == ('scissors')){
+            h1.innerText = win
+            playerScore.innerText++
+            playerScoreCounter++
+            winning()  
+        }
+        else {
+            h1.innerText = draw
+        }
+        gameOver()
     }
     function playPaper(){ 
         let computerSelection = computerPlay()
@@ -124,6 +87,59 @@ let enemyScoreCounter = 0
         }
         gameOver()
     }
+
+    function gameOver(){
+        if (playerScoreCounter == 5){
+            h1.innerText = 'You saved the day!'
+            paper.removeEventListener('click',playPaper)
+            rock.removeEventListener('click', playRock)
+            scissors.removeEventListener('click', playScissors)
+            nextGame()
+        }
+        if (enemyScoreCounter == 5){
+            h1.innerText = 'Abbandon all hope!!!'
+            paper.removeEventListener('click',playPaper)
+            rock.removeEventListener('click', playRock)
+            scissors.removeEventListener('click', playScissors)
+            nextGame()
+        }
+    }
+
+    function reset(){
+        h1.innerText = 'Good luck! AGAIN!!!'
+        playerScore.innerText = 0
+        enemyScore.innerText  = 0
+        playerScoreCounter = 0
+        enemyScoreCounter = 0
+        paper.addEventListener('click',playPaper)
+        rock.addEventListener('click', playRock)
+        scissors.addEventListener('click', playScissors)
+        tie()
+        body.removeChild(playAgain)
+    }
+
+    function nextGame(){
+        
+        playAgain.innerText= "Play Again"
+        playAgain.setAttribute("id", "play-again")
+        document.body.appendChild(playAgain)
+        playAgain.addEventListener('click', reset)
+    }
+
+    function loosing(){
+        playerScore.style.fontSize = "50px"
+        enemyScore.style.fontSize = "60px"
+    }
+    function winning(){
+        playerScore.style.fontSize = "60px"
+        enemyScore.style.fontSize = "50px"
+    }
+    function tie(){
+        playerScore.style.fontSize = "50px"
+        enemyScore.style.fontSize = "50px"       
+    }
+ 
+    
  
 
 paper.addEventListener('click',playPaper)
